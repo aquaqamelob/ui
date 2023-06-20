c9ac15307ca743622cb06c576fe3b5c200929b2b
aquaqamelob <127335511+aquaqamelob@users.noreply.github.com>
2023-06-20 10:37:58 +0200

    init

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import { Config } from "tailwindcss"
+
+import { shadcnPlugin } from "./lib/shadcn-plugin"
+
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: ["app/**/*.{ts,tsx}", "components/**/*.{ts,tsx}"],
+
+  plugins: [shadcnPlugin],
+} satisfies Config
+
+export default config
